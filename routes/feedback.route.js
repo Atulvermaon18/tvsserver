62d7c08ad470a36177328d02c10e3917897a7add
Atul <>
2019-04-23 19:24:05 +0530

    survey changes

--- routes/feedback.route.js
+++ routes/feedback.route.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const FeedbackController = require('../controllers/feedback.controller');
-const ConfController = require('../controllers/conference.controller');
 const TVSController = require('../controllers/tvsfeedback.controller');
 const Login = require('../controllers/admin.controller');
 const Mailer = require('../_helpers/mail.js');
 const middleware = require('../_helpers/middleware');
-
-
-router.post('/create', FeedbackController.feedback_create);
-router.get('/read', middleware.checkToken, FeedbackController.feedback_read); //Admin Access only
-router.post('/filter', middleware.checkToken, FeedbackController.filter); //Admin Access only
-router.post('/dateFilter', middleware.checkToken, FeedbackController.dateFilter); //Admin Access only
-router.post('/detailedFeedback', middleware.checkToken, FeedbackController.detailedFeedback); //Admin Access only
-
-router.post('/confCreate', ConfController.conf_create);
-router.get('/confRead', middleware.checkToken, ConfController.conf_read);//Admin Access only
-
 
 router.post('/ITCreate', TVSController.IT_create);
 router.post('/HRCreate', TVSController.HR_create);
