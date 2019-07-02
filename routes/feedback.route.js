a19d960768d5969d4d20f385f54e9f95c0180a38
Atul <>
2019-07-02 14:23:16 +0530

    update

--- routes/feedback.route.js
+++ routes/feedback.route.js
@@ -8,20 +8,20 @@ const middleware = require('../_helpers/middleware');
 
 
 router.post('/NewJoining', TVSController.NewJoining_create);
-router.get('/ITRead', middleware.checkToken, TVSController.TVS_read);//Admin Access only
+router.get('/ITRead', TVSController.TVS_read);//Admin Access only
 router.get('/export', TVSController.csvExport);//Admin Access only
 
 router.post('/authenticate', Login.authenticateUser);
 router.post('/changePassword', middleware.checkToken, Login.changeUserPassword);//Admin Access only
 
-router.post('/revertMail', middleware.checkToken, Mailer.revert)//Admin Access only
-router.post('/inviteMail', middleware.checkToken, Mailer.invite)//Admin Access only
+router.post('/revertMail', Mailer.revert)//Admin Access only
+router.post('/inviteMail', Mailer.invite)//Admin Access only
 
 router.post('/addQuestions', TVSController.Questions_create);
 router.get('/viewQuestions', TVSController.Questions_fetch);
 router.patch('/updateQuestions', TVSController.Questions_Update);
 
 router.post('/sendPassword', Mailer.sendPassword);
-router.post('/addMoreEmails', middleware.checkToken, Login.addMoreEmails);//Admin Access only
+router.post('/addMoreEmails', Login.addMoreEmails);//Admin Access only
 
 module.exports = router;
